06cc2f957dcc1ae554ceb20f8f898b7f9114237e
ilhoon <rival6821@naver.com>
2020-07-26 15:45:09 +0900

    필요없는 라우터 삭제

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,10 +1,12 @@
 var socket = io.connect("http://localhost:3000");
+
 socket.on("chat message", function (data) {
   console.log(data);
   var li = document.createElement("LI");
   li.textContent = data;
   document.getElementById("boxList").appendChild(li);
 });
+
 function chatMessage() {
   var $textInput = document.getElementById("textInput");
   socket.emit("chat message", $textInput.value);
